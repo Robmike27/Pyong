859aaca6a751b9a2899837a94d34d16b6623c891
Robmike27 <Robomike27theslime@gmail.com>
2022-04-08 08:58:22 -0700

    updated physics

--- supergame/gudgame.py
+++ supergame/gudgame.py
@@ -19,8 +19,12 @@ moving_2_down = False
 
 player_1_location = [50,170]
 player_2_location = [430,170]
+
 player_1_rect = pygame.Rect(player_1_location[0], player_1_location[1], 25, 50)
 player_2_rect = pygame.Rect(player_2_location[0], player_2_location[1], 25, 50)
+
+player_1_pad = pygame.Rect(player_1_location[0] + 13, player_1_location[1], 2, 50)
+player_2_pad = pygame.Rect(player_2_location[0] - 13, player_2_location[1], 2, 50)
 
 ball_position = [240,170]
 ball_rect = pygame.Rect(ball_position[0],ball_position[1],20,20)
@@ -28,32 +32,55 @@ ball_rect = pygame.Rect(ball_position[0],ball_position[1],20,20)
 ball_y_momentum = -.5
 ball_x_momentum = -1
 
-redWin_image = pygame.image.load("red_wins.png",)
-bluWin_image = pygame.image.load("blue_wins.png",)
+redWin_image = pygame.image.load("./supergame/red_wins.png",)
+bluWin_image = pygame.image.load("./supergame/blue_wins.png",)
 
 while True: #game loop
     screen.fill((0,0,0))
 
     player_1_rect.y = player_1_location[1]
-
     player_2_rect.y = player_2_location[1]
 
+    
+    player_1_top_pad = pygame.Rect(player_1_location[0], player_1_location[1] - 3, 30, 5)
+    player_1_bottom_pad = pygame.Rect(player_1_location[0], player_1_location[1] + 50, 30, 5)
+    player_1_front_pad = pygame.Rect(player_1_location[0] + 21, player_1_location[1] + 5, 1, 40)
+
+    player_2_top_pad = pygame.Rect(player_2_location[0], player_2_location[1] - 3, 30, 5)
+    player_2_bottom_pad = pygame.Rect(player_2_location[0], player_2_location[1] + 50, 30, 5)
+    player_2_front_pad = pygame.Rect(player_2_location[0] - 2, player_2_location[1], 1, 50)
+
     #draw player 1
+    pygame.draw.rect(screen,(25,200,0), player_2_top_pad)
+    pygame.draw.rect(screen,(200,0,0), player_2_bottom_pad)
     pygame.draw.rect(screen,(255,0,0), player_1_rect)
+    pygame.draw.rect(screen,(255,50,50), player_2_front_pad)
 
     #draw player 2
     pygame.draw.rect(screen,(0,0,255), player_2_rect)
     
     #player 1 and 2 paddle hit.
-    if player_1_rect.colliderect(ball_rect):
+    
+    if (player_1_top_pad.colliderect(ball_rect) or player_2_top_pad.colliderect(ball_rect)) and (ball_y_momentum > 0):
+        ball_position[1] -= 1
+        ball_y_momentum = ball_y_momentum * -1
+    
+    if player_1_bottom_pad.colliderect(ball_rect) or player_2_bottom_pad.colliderect(ball_rect):
+        ball_position[1] += 1
+        ball_y_momentum = ball_y_momentum * -1
+    
+    if player_1_front_pad.colliderect(ball_rect):
             pygame.draw.rect(screen,(0,255,0),ball_rect)
+            ball_position[0] += 3
             ball_x_momentum = ball_x_momentum * -1.01
             ball_y_momentum = ball_y_momentum * 1.01
 
-    if player_2_rect.colliderect(ball_rect):
+    if player_2_front_pad.colliderect(ball_rect):
             pygame.draw.rect(screen,(0,255,0),ball_rect)
+            ball_position[0] -= 3
             ball_x_momentum = ball_x_momentum * -1.01
             ball_y_momentum = ball_y_momentum * 1.01
+
 
     #moves player 1 paddle
     if moving_up == True:
@@ -89,6 +116,13 @@ while True: #game loop
     if ball_position[1] > int(WINDOW_SIZE[1])-20:
         ball_y_momentum = ball_y_momentum * -1
     
+    if ball_x_momentum > 10:
+        ball_x_momentum = 10
+    
+    if ball_y_momentum > 10:
+        ball_y_momentum = 10
+        
+
     pygame.draw.rect(screen,(255,255,255),ball_rect)
     ball_position[0] += ball_x_momentum
     ball_rect.x = ball_position[0]
